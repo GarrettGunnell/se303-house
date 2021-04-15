8d7ed0667b38ce54e3effc218d9232598cd61329
Garrett Gunnell <gunnellg@oregonstate.edu>
2021-04-15 10:22:40 -0700

    Add new case for 2nd line of the house that jack built

--- lib/house.rb
+++ lib/house.rb
@@ -1,5 +1,10 @@
 class House
     def line(n)
-        "This is the house that Jack built.\n"
+        case n
+        when 1
+            "This is the house that Jack built.\n"
+        when 2
+            "This is the malt that lay in the house that Jack built.\n"
+        end
     end
 end
