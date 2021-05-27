47e43452b6965744347680b2a5c5f6fd3be028af
Garrett Gunnell <gunnellg@oregonstate.edu>
2021-05-27 12:15:21 -0700

    Verses method now constructs verses from verbs and subjects arrays

--- lib/house.rb
+++ lib/house.rb
@@ -1,20 +1,4 @@
 class House
-    def verses 
-        [
-            "the horse and the hound and the horn that belonged to ",
-            "the farmer sowing his corn that kept ",
-            "the rooster that crowed in the morn that woke ",
-            "the priest all shaven and shorn that married ",
-            "the man all tattered and torn that kissed ",
-            "the maiden all forlorn that milked ",
-            "the cow with the crumpled horn that tossed ",
-            "the dog that worried ",
-            "the cat that killed ",
-            "the rat that ate ",
-            "the malt that lay in ",
-        ]
-    end
-
     def subjects
         [
             "the horse and the hound and the horn that ",
@@ -49,6 +33,10 @@ class House
 
     def initialize(prefix="This is")
         @prefix = prefix
+    end
+
+    def verses
+        (0..10).collect { |i| subjects[i] + verbs[i] }
     end
 
     def phrases(n)
