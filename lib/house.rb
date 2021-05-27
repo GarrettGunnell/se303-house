79acc620db14a366634227596a1c99fe43768293
Garrett Gunnell <gunnellg@oregonstate.edu>
2021-05-27 11:58:56 -0700

    Recite the house that jack built with random subjects and verbs

--- lib/house.rb
+++ lib/house.rb
@@ -37,3 +37,45 @@ class RandomHouse < House
         verses.slice(12 - n, 12).shuffle.join("")
     end
 end
+
+
+class RandomPhraseHouse < House
+    def subjects
+        [
+            "the horse and the hound and the horn that ",
+            "the farmer sowing his corn that ",
+            "the rooster that crowed in the morn that ",
+            "the priest all shaven and shorn that ",
+            "the man all tattered and torn that ",
+            "the maiden all forlorn that ",
+            "the cow with the crumpled horn that ",
+            "the dog that ",
+            "the cat that ",
+            "the rat that ",
+            "the malt that ",
+        ]
+    end
+
+    def verbs
+        [
+            "belonged to ",
+            "kept ",
+            "woke ",
+            "married ",
+            "kissed ",
+            "milked ",
+            "tossed ",
+            "worried ",
+            "killed ",
+            "ate ",
+            "lay in ",
+        ]
+    end
+
+    def phrases(n)
+        s = subjects.shuffle.slice(0, n - 1)
+        v = verbs.shuffle.slice(0, n - 1)
+
+        0.upto(n - 2).collect { |i| s[i] + v[i] }.join("")
+    end
+end
