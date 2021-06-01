e892555239bd23ef0b3795fde3e347fac43438dc
Garrett Gunnell <gunnellg@oregonstate.edu>
2021-06-01 10:00:00 -0700

    Fix methods not being called

--- lib/house.rb
+++ lib/house.rb
@@ -17,7 +17,7 @@ class Verses
     def initialize(behaviors: [])
         @subjects = ["malt", "rat", "cat", "dog", "cow with the crumpled horn", "maiden all forlorn", "man all tattered and torn", "priest all shaven and shorn", "rooster that crowed in the morn", "farmer sowing his corn", "horse and the hound and the horn"]
         @verbs = ["lay in", "ate", "killed", "worried", "tossed", "milked", "kissed", "married", "woke", "kept", "belonged to"]
-        behaviors.each { |x| method(x) }
+        behaviors.each { |x| method(x).() }
     end
 
     def random_verses
